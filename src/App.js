e8dd63e525a62123e17e81368c06b83489866258
Pavan Rane <ranepavanrp@gmail.com>
2023-10-15 18:56:26 +0530

    created a form structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {QRCodeSVG} from 'qrcode.react';
 
 const App = () => {
+
+  const [input, setInput] = useState("")
+
   return (
-    <div>
+    <div style={{}}>
+      <h2>Qr Code Generator</h2>
+      <form action="submit">
+        <input type="text" />
+        <br />
+        <input type='submit' />
+      </form>
+      <br />
       <QRCodeSVG value="pavan"/>
     </div>
   )
